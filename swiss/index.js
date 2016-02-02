b83059a9328e57f48d1e6fceff3ea18768036d5f
ling5656 <563290954@qq.com>
2016-02-02 22:54:10 +0800

    xx

--- swiss/index.js
+++ swiss/index.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function () {
+﻿$(function () {
     var openEl = null;
     $('#ul1 li').click(function () {
         openEl = $(this);
@@ -32,38 +32,60 @@
     $('no').click(function () {
         $('#pageHome').addClass('open');
     });
-    $('#submit').click(function (e) { e = e || window.event;
-        e.preventDefault ? e.preventDefault() : e.returnValue = false;
 
+    $('#submit').click(function (e) {
+        e = e || window.event;
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
         $('#pageList').show();
 
-        var oText = document.querySelectorAll(".pres ul li input[type='text']");
-        for (var i = 0; i < oText.length; i++) {
-            //清空前后的空格
-            if (oText[i].value.replace(/(^\s*)|(\s*$)/g, "") == "") {
-                alert("请输入" + oText[i].getAttribute("val") + "后进行提交操作!");
-                return;
-            }
-            if (oText[i].getAttribute("val") == "手机号") {
-                if (!(/^1[3|4|5|8]\d{9}$/.test(oText[i].value))) {
-                    alert("输入的手机号格式不正确!");
-                    return;
-                }
-            }
-            if (oText[i].getAttribute("val") == "电子邮件") {
-                if (!(/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(oText[i].value))) {
-                    alert("输入的电子邮件格式不正确!");
-                    return;
-                }
-            }
-        }
+        //var oText = document.querySelectorAll(".pres ul li input[type='text']");
+        //for (var i = 0; i < oText.length; i++) {
+        //    //清空前后的空格
+        //    if (oText[i].value.replace(/(^\s*)|(\s*$)/g, "") == "") {
+        //        alert("请输入" + oText[i].getAttribute("val") + "后进行提交操作!");
+        //        return;
+        //    }
+        //    if (oText[i].getAttribute("val") == "手机号") {
+        //        if (!(/^1[3|4|5|8]\d{9}$/.test(oText[i].value))) {
+        //            alert("输入的手机号格式不正确!");
+        //            return;
+        //        }
+        //    }
+        //    if (oText[i].getAttribute("val") == "电子邮件") {
+        //        if (!(/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(oText[i].value))) {
+        //            alert("输入的电子邮件格式不正确!");
+        //            return;
+        //        }
+        //    }
+        //}
 
         setTimeout(function () {
             $('#pageInfo .pres').addClass('zoomOut');
             $('#pageList').addClass('open');
             $('body').css({overflow: 'scroll'})
         }, 0);
+
+        //第四页
+        $("div.homeDir").click(function () {
+            $("#detailsPage").show();
+            setTimeout(function () {
+                $('#detailsPage').addClass('open');
+                $('#pageList').addClass('close');
+                $('body').css({overflow: 'hidden'});
+            }, 400);
+        });
+        $("#det_close").click(function () {
+            $("#pageList").show();
+            setTimeout(function () {
+                $('#detailsPage').addClass('close');
+                $('#pageList').removeClass('close');
+                $('#detailsPage').removeClass('open');
+                $('#detailsPage').removeClass('close');
+                $('body').css({overflow: 'scroll'});
+            }, 400);
+        });
     });
+
 });
 
 
